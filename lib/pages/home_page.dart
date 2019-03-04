526e1538a1db42a288dbde3eb97ab0c707da85c6
e50jaywe <e50jaywe@163.com>
2019-03-04 09:54:47 +0800

    商品分类导航的编写

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -22,9 +22,14 @@ class _HomePageState extends State<HomePage> {
               var data = json.decode(snapshot.data.toString());
               List<Map> swiperDataList =
                   (data['data']['slides'] as List).cast(); // 顶部轮播组件数
+              List<Map> navigatorList = (data['data']['category'] as List).cast(); // 导航组件
+              if(navigatorList.length>10){
+                navigatorList.removeRange(10, navigatorList.length);
+              }
               return Column(
                 children: <Widget>[
                   SwiperDiy(swiperDataList: swiperDataList),
+                   TopNavigator(navigatorList: navigatorList)
                 ],
               );
             } else {
@@ -42,7 +47,7 @@ class SwiperDiy extends StatelessWidget {
   SwiperDiy({Key key, this.swiperDataList}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    ScreenUtil.instance =ScreenUtil(width: 750,height: 1334)..init(context);
+    
     return Container(
       height: ScreenUtil().setHeight(333),
       width:ScreenUtil().setWidth(750),
@@ -54,6 +59,38 @@ class SwiperDiy extends StatelessWidget {
         itemCount: swiperDataList.length,
         pagination: new SwiperPagination(),
         autoplay: true,
+      ),
+    );
+  }
+}
+
+class TopNavigator extends StatelessWidget {
+  final List navigatorList;
+
+  TopNavigator({Key key, this.navigatorList}) : super(key: key);
+
+  Widget _gridViewItemUI(BuildContext context,item){
+    return InkWell(
+      onTap: (){print('点击了导航');},
+      child: Column(
+        children: <Widget>[
+          Image.network(item['image'],width: ScreenUtil().setWidth(95),),
+          Text(item['mallCategoryName'])
+        ],
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: ScreenUtil().setHeight(320),
+      padding: EdgeInsets.all(3.0),
+      child: GridView.count(
+        crossAxisCount: 5,
+        padding: EdgeInsets.all(4.0),
+        children: navigatorList.map((item){
+          return _gridViewItemUI(context, item);
+        }).toList(),
       ),
     );
   }
