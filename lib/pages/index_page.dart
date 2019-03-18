5777fb41d5b3f416baec7993d34bb06cefe5dc4a
e50jaywe <e50jaywe@163.com>
2019-03-18 11:26:27 +0800

    添加页面状态管理

--- lib/pages/index_page.dart
+++ lib/pages/index_page.dart
@@ -31,7 +31,7 @@ class _IndexPageState extends State<IndexPage> {
     ),
   ];
 
-  final List tabBodies  = [
+  final List<Widget> tabBodies  = [
     HomePage(),
     CategoryPage(),
     CartPage(),
@@ -62,7 +62,10 @@ class _IndexPageState extends State<IndexPage> {
           });
         },
       ),
-      body: currentPage,
+      body: IndexedStack(
+        index: pageIndex,
+        children: tabBodies
+      ),
     );
   }
 }
